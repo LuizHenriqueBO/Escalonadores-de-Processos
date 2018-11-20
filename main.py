7d77a9f22c9a0a6a272f61dfba9c017a3eaa3b23
souzalucas <lsantos.2016@alunos.utfpr.edu.br>
2018-11-20 01:47:57 -0200

    Interface

--- main.py
+++ main.py
@@ -17,30 +17,49 @@ def leitor(file_name):
 
 
 def main():
-    fila_processos = leitor("processos.txt")
+    print("Algoritmos de escalonamento")
 
-    gp = Gerenciador()
-    for processo in fila_processos:
-        bcp = BCP()
-        bcp.set_id(processo[0])
-        bcp.set_tempo_CPU(processo[1])                        # setando os dados no bcp
-        bcp.set_prioridade(processo[2])
-        bcp.set_tempo_chegada(processo[3])
-        bcp.set_fila_io(processo[4:])
+    opcao = -1
+    while(opcao != '0'):
+
+        fila_processos = leitor("processos.txt")
+
+        gp = Gerenciador()
+        for processo in fila_processos:
+            bcp = BCP()
+            bcp.set_id(processo[0])
+            bcp.set_tempo_CPU(processo[1])                        # setando os dados no bcp
+            bcp.set_prioridade(processo[2])
+            bcp.set_tempo_chegada(processo[3])
+            bcp.set_fila_io(processo[4:])
+            
+            gp.add_fila_processos(bcp)
         
-        gp.add_fila_processos(bcp)
+        for i in gp.get_fila_processos():
+            #i.fila_IO = [int(k) for k in i.fila_IO]
+            print(i.get_id(), i.get_tempo_chegada(), i.get_fila_io(), i.get_tempo_CPU())
+            
+        escalonador = Escalonador()
 
+        print("Escalonadores")
+        print("[1] FIFO")
+        print("[2] SJF")
+        print("[3] Prioridade")
+        print("[4] Round Robin")
+        print("[0] Sair")
+
+        opcao = input()
+        if(opcao == '1'):
+            escalonador.fifo(gp)
+        if(opcao == '2'):
+            escalonador.sjf(gp)
+        if(opcao == '3'):
+            escalonador.prioridade(gp)
+        if(opcao == '4'):
+            escalonador.RoundRobin(gp)
         
-
-    
-    for i in gp.get_fila_processos():
-        #i.fila_IO = [int(k) for k in i.fila_IO]
-        print(i.get_id(), i.get_tempo_chegada(), i.get_fila_io(), i.get_tempo_CPU())
-
-    escalonador = Escalonador()
-    escalonador.prioridade(gp)
-    print(escalonador.tempo_total())
-    'Tempo total {}'.format(escalonador.tempo_total())
+        print(escalonador.tempo_total())
+        'Tempo total {}'.format(escalonador.tempo_total())
 
 if __name__ == '__main__':
 	main()
